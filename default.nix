b5329fa95204836b73ce2668a239efb0aab09639
camilo-delga <santiago.diaz36033756@google.com>
2025-12-18 20:59:21 -0300

    Fix: Use direct  instead of placeholder in heredoc

--- default.nix
+++ default.nix
@@ -6,7 +6,6 @@
   runtimeDeps,
   wrapGAppsHook3,
   pkg-config,
-  writeShellScript,
   gtk3,
   gtk4,
   glib,
@@ -19,12 +18,6 @@
   tabler-icons-font,
 }:
 
-let
-  ax-shell-unwrapped = writeShellScript "ax-shell-unwrapped" ''
-    cd ${placeholder "out"}/share/ax-shell
-    exec ${ax-shell-python}/bin/python -m main "$@"
-  '';
-in
 stdenv.mkDerivation {
   pname = "ax-shell";
   version = "unstable-${self.shortRev or "dirty"}";
@@ -53,27 +46,8 @@ stdenv.mkDerivation {
     mkdir -p $out/share/ax-shell
     mkdir -p $out/bin
     cp -r ./* $out/share/ax-shell/
-    cp ${ax-shell-unwrapped} $out/bin/.ax-shell-unwrapped
-    chmod +x $out/bin/.ax-shell-unwrapped
-    runHook postInstall;
-  '';
-
-  preFixup = ''
-    gappsWrapperArgs+=(--set AX_SHELL_WALLPAPERS_DIR_DEFAULT "$out/share/ax-shell/assets/wallpapers_example")
-    gappsWrapperArgs+=(--set FABRIC_CSS_PATH "$out/share/ax-shell/main.css")
-    gappsWrapperArgs+=(--prefix PATH : "${lib.makeBinPath runtimeDeps}")
-    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${tabler-icons-font}/share")
-    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${adwaita-icon-theme}/share")
-  '';
-  
-  postFixup = ''
-    wrapGApp $out/bin/.ax-shell-unwrapped
-    mv $out/bin/.ax-shell-unwrapped $out/bin/ax-shell
-  '';
-
-  meta = {
-    description = "A custom, flake-based package for Ax-Shell.";
-    homepage = "https://github.com/poogas/Ax-Shell";
-    license = lib.licenses.mit;
-  };
-}
+    
+    cat > $out/bin/.ax-shell-unwrapped <<EOF
+#!/bin/sh
+export PYTHONPATH="$out/share/ax-shell:\''${PYTHONPATH}"
+exec ${ax-shell-python}/bin/python -m main "\$@"
