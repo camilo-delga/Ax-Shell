45eaff469e3941c5870966068c19958ef5bc30bc
camilo-delga <santiago.diaz36033756@google.com>
2025-12-18 18:29:32 -0300

    Fix: Add gtk3 and gtk4 to pygobject buildInputs for typelib support

--- nix/pkgs/pygobject.nix
+++ nix/pkgs/pygobject.nix
@@ -8,6 +8,48 @@
   gobject-introspection,
   pycairo,
   cairo,
+  gtk3,
+  gtk4,
+  meson,
+  ninja,
+  python,
+}:
+
+buildPythonPackage rec {
+  pname = "pygobject";
+  version = "3.52.3";
+  format = "other";
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.gz";
+    sha256 = "sha256-AOQn0pHpV0Yqj61lmp+ci+d2/4Kot2vfQC8eruwIbYI=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    ninja
+    gobject-introspection
+  ];
+
+  buildInputs = [
+    cairo
+    glib
+    gtk3
+    gtk4
+  ];
+
+  propagatedBuildInputs = [ pycairo ];
+}{
+  lib,
+  stdenv,
+  fetchurl,
+  buildPythonPackage,
+  pkg-config,
+  glib,
+  gobject-introspection,
+  pycairo,
+  cairo,
   meson,
   ninja,
   python,
