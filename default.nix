7899da6c11d3ba7af2a9ab2291e61697b465c88e
camilo-delga <santiago.diaz36033756@google.com>
2025-12-18 21:08:35 -0300

    Fix: Correct heredoc syntax with proper quoting

--- default.nix
+++ default.nix
@@ -47,7 +47,32 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     cp -r ./* $out/share/ax-shell/
     
-    cat > $out/bin/.ax-shell-unwrapped <<EOF
+    cat > $out/bin/.ax-shell-unwrapped << 'UNWRAPPED'
 #!/bin/sh
-export PYTHONPATH="$out/share/ax-shell:\''${PYTHONPATH}"
-exec ${ax-shell-python}/bin/python -m main "\$@"
+export PYTHONPATH="$out/share/ax-shell:''${PYTHONPATH}"
+exec ${ax-shell-python}/bin/python -m main "$@"
+UNWRAPPED
+    chmod +x $out/bin/.ax-shell-unwrapped
+    
+    runHook postInstall;
+  '';
+
+  preFixup = ''
+    gappsWrapperArgs+=(--set AX_SHELL_WALLPAPERS_DIR_DEFAULT "$out/share/ax-shell/assets/wallpapers_example")
+    gappsWrapperArgs+=(--set FABRIC_CSS_PATH "$out/share/ax-shell/main.css")
+    gappsWrapperArgs+=(--prefix PATH : "${lib.makeBinPath runtimeDeps}")
+    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${tabler-icons-font}/share")
+    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${adwaita-icon-theme}/share")
+  '';
+  
+  postFixup = ''
+    wrapGApp $out/bin/.ax-shell-unwrapped
+    mv $out/bin/.ax-shell-unwrapped $out/bin/ax-shell
+  '';
+
+  meta = {
+    description = "A custom, flake-based package for Ax-Shell.";
+    homepage = "https://github.com/poogas/Ax-Shell";
+    license = lib.licenses.mit;
+  };
+}
